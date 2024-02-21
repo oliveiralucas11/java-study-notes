56a5db2c44e6642418eff7d54181bfa687ba04ef
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-02-21 18:46:07 -0400

    new basic concepts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o ScreenMatch");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        // Variaveis do Tipo inteiro
+
+        int ano = 2022;
+        int anoDeLancamento;
+        anoDeLancamento = 2023; // Pode ser declarada e passada o valor a ela dessas duas formas
+
+        System.out.println("Ano de lançamento: " + anoDeLancamento); // Podendo ser concatenado com um texto
+
+        // Tipo Boolean
+
+        boolean incluidoNoPlano = false;
+        boolean incluidoNoPlano2 = true;
+
+        boolean incluidoNoPlano3;
+        incluidoNoPlano3 = false;
+
+        // Tipo Double
+
+        double notaDoFilme = 8.1;
+
+        double notaDoFilme2;
+        notaDoFilme2 = 9.5;
+
     }
 }
