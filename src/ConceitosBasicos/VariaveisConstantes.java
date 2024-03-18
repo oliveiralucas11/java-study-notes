9c24354c7d0bbb078cc5b72edba62e59210190bd
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-03-18 13:02:09 -0400

    mais anotacoes de estudo

--- src/ConceitosBasicos/VariaveisConstantes.java
+++ src/ConceitosBasicos/VariaveisConstantes.java
@@ -1,7 +1,16 @@
 package ConceitosBasicos;
 
+import java.util.Scanner; // Está avisando o java que vai ser usada a funcionalidade scanner
+
 public class VariaveisConstantes {
     public static void main(String[] args) {
-        System.out.println("Ola");
+        // Algoritimos simples, usando uma funcionalidade que serve para fazer leitura de informações
+
+        Scanner scanner = new Scanner(System.in); // Forma de fazer a leitura de um numero que o usuario digitar no console
+
+        double valorProduto = scanner.nextDouble(); // Declarado
     }
 }
+
+
+
