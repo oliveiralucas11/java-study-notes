9b4a6e68e39053b3e9dc35ba499bb9c6a6efd064
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-06-06 17:06:59 -0400

    exercicios

---
+++ src/estudosdevdojo/conceitosbasicos/Aula01TiposPrimitivos.java
@@ -0,0 +1,42 @@
+package estudosdevdojo.conceitosbasicos;
+
+public class Aula01TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        //Tipos Primitivos
+        //Sao tipos que vao guardar na memoria um valor especifico como uma variavel ou constante eles sao:
+
+        // int, double, float, char, byte, short, long, boolean
+
+        //Para criar uma variavel precisamos primeiro dizer o tipo dela e em seguida um nome
+
+        //Tipo int, long
+        int age = 24;
+        int fatherAge = 49;
+        long numeroGrande = 100000;
+
+
+        //Tipo double, float, byte, short, long, boolean, char
+        double salarioDouble = 2000.20;
+        float salarioFloat = 2500.32F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A'; // Somente 1 caractere como tipo de sexo masculino ou feminino
+
+        //String tipo de referencia, nao sendo um tipo primitivo e sim uma classe
+        String textoGrande = "Um grande texto";
+        String nomePessoa = "Lucas Oliveira";
+
+
+        System.out.println(textoGrande);
+        System.out.println("Oi meu nome é " + nomePessoa);
+        System.out.println(age);
+        System.out.println("A idade é: " + fatherAge + " anos");
+
+
+
+    }
+}
---
+++ src/estudosdevdojo/conceitosbasicos/Aula02TiposPrimitivosExercicio.java
@@ -0,0 +1,20 @@
+package estudosdevdojo.conceitosbasicos;
+
+/*
+* Pratica
+*
+* Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+* Eu <nome>, morando no endereco <endereco>,
+* confirmo que recebi o salario de <salario>, na data <data>
+* */
+
+public class Aula02TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
