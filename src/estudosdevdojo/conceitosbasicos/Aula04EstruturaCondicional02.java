e8e7c875befcf4cf6bcff481335dd643bb2bd789
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2024-06-09 19:06:52 -0400

    mais formas de condicionais

---
+++ src/estudosdevdojo/conceitosbasicos/Aula04EstruturaCondicional02.java
@@ -0,0 +1,35 @@
+package estudosdevdojo.conceitosbasicos;
+
+public class Aula04EstruturaCondicional02 {
+
+    public static void main(String[] args) {
+
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade >= 18 categoria adulto
+
+        int idade = 20;
+        String categoria;
+
+        if (idade < 15) {
+            System.out.println("Categoria Infantil");
+        } else if (idade >= 15 && idade < 18) {
+            System.out.println("Categoria Juvenil");
+        } else {
+            System.out.println("Categoria adulto");
+        }
+
+
+        // Forma diferente de fazer
+        if (idade < 15) {
+            categoria = "Categoria Infantil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Categoria Juvenil";
+        }else {
+            categoria = "Adulto";
+        }
+        System.out.println(categoria);
+
+
+    }
+}
